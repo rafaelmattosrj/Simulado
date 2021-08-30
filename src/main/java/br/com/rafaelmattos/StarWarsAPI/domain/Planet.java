2dcf7a4d3482f09fbdfbc7c545b6b5c52214d2fa
Rafael Mattos <rafaelrj@live.com>
2021-08-30 13:25:32 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/br/com/rafaelmattos/StarWarsAPI/domain/Planet.java
+++ src/main/java/br/com/rafaelmattos/StarWarsAPI/domain/Planet.java
@@ -2,10 +2,16 @@ package br.com.rafaelmattos.StarWarsAPI.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 public class Planet implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String climates;
