431fed2d20fdb368793a523ac81ff03019464403
Rafael Mattos <rafaelrj@live.com>
2021-08-30 12:00:29 -0300

    Projeto Criado

---
+++ src/test/java/br/com/rafaelmattos/StarWarsAPI/StarWarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmattos.StarWarsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
